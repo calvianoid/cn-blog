0b1713e62d14ce74f30abdc6aa14853a1b979431
Calviano Nathanael <calvianoid@gmail.com>
2022-01-20 01:01:40 +0700

    catatan

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,8 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-
-    // protected $fillable = ['title', 'excerpt', 'body'];
     protected $guarded = ['id'];
     protected $with = ['category', 'author'];
 
--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,29 @@ use Illuminate\Http\Request;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Menampilkan Halaman Home
+Route::get('/', function(){
+    return view('home', [
+        "active" => "home",
+        "title" => "Home"
+    ]);
+});
 
-Route::get('/', [HomeController::class, 'index']);
+// Menampilkan Halaman About
+Route::get('/about', function(){
+    return view('about', [
+        "title" => "About",
+        "active" => "about",
+        "name" => "Calviano Nathanel",
+        "email" => "calvianoid@gmail.com",
+        "image" => "logo.png"
+    ]);
+});
 
-Route::get('/about', [AboutController::class, 'index']);
+// Menampilkan Halaman Dashboard
+Route::get('/dashboard', function(){
+    return view('dashboard.index');
+})->middleware('auth');
 
 Route::get('/posts', [PostController::class, 'index']); // Menampilkan Halaman All Post
 Route::get('posts/{post:slug}', [PostController::class, 'show']); // Menampilkan Halaman Single Post
@@ -32,14 +51,9 @@ Route::get('/categories', [CategoryController::class, 'index']); // Menampilkan 
 
 Route::get('/login', [LoginController::class, 'index'])->middleware('guest')->name('login'); // Menampilkan Halaman Login
 Route::post('/login', [LoginController::class, 'authenticate']); // Authenticate Login
-Route::post('/logout', [LoginController::class, 'logout']);
+Route::post('/logout', [LoginController::class, 'logout']); //Logout
 
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest'); // Menampilkan Halaman Register
 Route::post('/register', [RegisterController::class, 'store']); // Menyimpan Data Register
 
-// Menampilkan Halaman Dashboard
-Route::get('/dashboard', function(){
-    return view('dashboard.index');
-})->middleware('auth');
-
-Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth'); //Store, Show, Edit, Update, Destroy My Post
